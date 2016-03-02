93c00b4ea4d8eef927164df471dc504273beb6bc
Ryan Struhl <rstruhl@brightcove.com>
2016-03-02 15:54:56 -0500

    adding mkdir to install for nvim

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.xdg_config}
 
 # Install all the vim plugins!
 git pull origin master
@@ -18,6 +19,9 @@ fi
 #vim
 ln -s ${BASEDIR}/vimrc ~/.vimrc
 ln -s ${BASEDIR}/vim/ ~/.vim
+
+#nvim
+
 
 #zsh
 ln -s ${BASEDIR}/aliases ~/.aliases.zsh
